a2b3b5e9b337f799d224cc1d1c1133ff7b8f816c
Dima Skorobogatov <lllekcl7up@gmail.com>
2023-03-20 17:28:28 +0300

    Update Lec

---
+++ Lessons/Lecture6/part1/Ex001_HashSet.java
@@ -0,0 +1,31 @@
+package JavaLesson.Lessons.Lecture6.part1;
+
+import java.util.*;
+// isEmpty() – проверка на пустоту.
+// add(V) – добавление элемента в коллекцию.
+// remove(V) – удаление элемента из коллекцию.
+// contains(V) – проверка на включение элемента в коллекции.
+// clear() – удаление всех элементов коллекции.
+// size() – возвращает количество элементов коллекции.
+
+// addAll(Coll) – объединение множеств.
+// retainAll(Coll) – пересечение множеств.
+// removeAll(Coll) – разность множеств.
+
+public class Ex001_HashSet {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(1); set.add(12); set.add(123);
+        set.add(1234); set.add(1234);
+        System.out.println(set.contains(12)); // true
+        set.add(null);
+        System.out.println(set.size()); // 5
+        System.out.println(set); // [null, 1, 1234, 123, 12]
+        set.remove(12);
+        for (var item : set) {
+            System.out.println(item); // null 1 1234 123
+        }
+        set.clear();
+        System.out.println(set); // []
+    }
+}
---
+++ Lessons/Lecture6/part1/Ex004_LinkedHashSet.java
@@ -0,0 +1,19 @@
+package JavaLesson.Lessons.Lecture6.part1;
+
+import java.util.*;
+// isEmpty() – проверка на пустоту.
+// add(V) – добавление элемента в коллекцию.
+// remove(V) – удаление элемента из коллекцию.
+// contains(V) – проверка на включение элемента в коллекции.
+// clear() – удаление всех элементов коллекции.
+// size() – возвращает количество элементов коллекции.
+public class Ex004_LinkedHashSet {
+    public static void main(String[] args) {
+        var a = new LinkedHashSet<>(Arrays.asList(1,7,2,3,6,4,5));
+        var b = new LinkedHashSet<>(Arrays.asList(13,3,17,7,2,11,5));
+        a.add(28);
+        System.out.println(a); // [1, 7, 2, 3, 6, 4, 5, 28]
+        System.out.println(b); // [13, 3, 17, 7, 2, 11, 5]
+    }
+
+}
